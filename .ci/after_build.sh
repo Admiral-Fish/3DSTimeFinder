38f3b1a4e7a823d70d6404ed77d07eb706c82dc5
Admiral-Fish <admiralfish420@gmail.com>
2021-10-03 12:29:14 -0700

    Tweak build steps

--- .ci/after_build.sh
+++ .ci/after_build.sh
@@ -3,11 +3,13 @@
 case $OS in
   linux)
   {
+    mv build/Source/Forms/3DSTimeFinder .
     zip -r 3DSTimeFinder-linux.zip 3DSTimeFinder
     sha256sum 3DSTimeFinder-linux.zip > 3DSTimeFinder-linux.zip.sha256
   } ;;  
   macOS)
   {
+    mv build/Source/Forms/3DSTimeFinder.app .
     PATH=$PATH:$HOME/Qt/6.1/macos/bin macdeployqt 3DSTimeFinder.app -dmg -verbose=2
     zip -r 3DSTimeFinder-macOS.zip 3DSTimeFinder.dmg
     shasum -a 256 3DSTimeFinder-macOS.zip > 3DSTimeFinder-macOS.zip.sha256
